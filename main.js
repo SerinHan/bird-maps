f0f08625618b231cb5f02cf363e3354fbf34e5fe
SerinHan <serinushan@gmail.com>
2020-05-12 13:26:13 -0700

    search specific address on map

--- main.js
+++ main.js
@@ -4,6 +4,26 @@ function initMap() {
     var uluru = { lat: -25.344, lng: 131.036 };
     var map = new google.maps.Map(
         document.getElementById("map"), { zoom: 4, center: uluru });
+    var geocoder = new google.maps.Geocoder();
+
+    document.getElementById("submit").addEventListener("click", function () {
+        geocodeAddress(geocoder, map);
+    });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById("address").value;
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
 }
 
 var srcString = "https://maps.googleapis.com/maps/api/js?key=" + mapsKey + "&callback=initMap";
