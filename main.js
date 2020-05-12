5d2360c1aef6f9252225f3f9d8c3cbe60b4a951d
SerinHan <serinushan@gmail.com>
2020-05-12 13:19:23 -0700

    added basic google map

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 var eBird = new Bird();
+
+function initMap() {
+    var uluru = { lat: -25.344, lng: 131.036 };
+    var map = new google.maps.Map(
+        document.getElementById("map"), { zoom: 4, center: uluru });
+}
+
+var srcString = "https://maps.googleapis.com/maps/api/js?key=" + mapsKey + "&callback=initMap";
+var mapScript = document.createElement("script");
+mapScript.src = srcString;
+mapScript.defer = true;
+mapScript.async = true;
+document.querySelector("body").append(mapScript);
